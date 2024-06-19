762b959fe3633e246b8d08107f196861fd6b6fa6
LinSta3032 <148768601+LinSta3032@users.noreply.github.com>
2024-06-19 19:41:39 +0800

    Add files via upload

---
+++ JS/videoChange.js
@@ -0,0 +1,45 @@
+function videoShow2() {
+    document.getElementById('v2').src = 'video/MC-D.mp4';
+    document.getElementById('i1').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i2').style.backgroundColor = 'rgb(61, 57, 56)';
+    document.getElementById('i3').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i4').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i2').style.border = 'white 2px solid';
+    document.getElementById('i1').style.border = null;
+    document.getElementById('i3').style.border = null;
+    document.getElementById('i4').style.border = null;
+}
+function videoShow1() {
+    document.getElementById('v2').src = 'video/MC-O.mp4';
+    document.getElementById('i2').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i1').style.backgroundColor = 'rgb(61, 57, 56)';
+    document.getElementById('i3').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i4').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i4').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i1').style.border = 'white 2px solid';
+    document.getElementById('i2').style.border = null;
+    document.getElementById('i3').style.border = null;
+    document.getElementById('i4').style.border = null;
+}
+function videoShow3() {
+    document.getElementById('v2').src = 'video/MC-L.mp4';
+    document.getElementById('i3').style.backgroundColor = 'rgb(61, 57, 56)';
+    document.getElementById('i1').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i2').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i4').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i3').style.border = 'white 2px solid';
+    document.getElementById('i2').style.border = null;
+    document.getElementById('i1').style.border = null;
+    document.getElementById('i4').style.border = null;
+}
+function videoShow4() {
+    document.getElementById('v2').src = 'video/MC-E.mp4';
+    document.getElementById('i4').style.backgroundColor = 'rgb(61, 57, 56)';
+    document.getElementById('i1').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i2').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i3').style.backgroundColor = 'rgb(38, 36, 35)';
+    document.getElementById('i4').style.border = 'white 2px solid';
+    document.getElementById('i2').style.border = null;
+    document.getElementById('i3').style.border = null;
+    document.getElementById('i1').style.border = null;
+}
